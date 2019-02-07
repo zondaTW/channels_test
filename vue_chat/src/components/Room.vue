72f9d5d645b88915c8a9cb104552852a6fe98276
zondaTW <u226699@gmail.com>
2019-02-07 23:54:28 +0800

    Add focus

--- vue_chat/src/components/Room.vue
+++ vue_chat/src/components/Room.vue
@@ -2,14 +2,14 @@
   <div id="chatSection" class="Room container">
     <div class="row h-15">
       <div class="col" >
-        <h1>Room <span class="badge badge-primary">{{roomName}}</span></h1>
+        <h1><span class="badge badge-primary">Room: {{roomName}}</span></h1>
       </div>
     </div>
-    <div id="chatLog" class="row h-75">
-      <textarea id="chat-log"></textarea>
+    <div class="row h-75">
+      <textarea id="chatLog" v-model="chatLogContent"></textarea>
     </div>
     <div class="row h-10">
-      <input type="text" v-model="inputContent" class="w-75"/>
+      <input type="text" v-model="inputContent" class="w-75" v-on:keyup.enter="sendMessage()" v-focus/>
       <button type="button" class="btn btn-primary w-25" @click="sendMessage()">Send</button>
     </div>
   </div>
@@ -20,27 +20,18 @@ export default {
   name: 'Room',
   data () {
     return {
-      roomName: '123',
+      roomName: 'test',
       hostAdr: '127.0.0.1:8081',
       chatSocket: null,
-      inputContent: ''
+      inputContent: '',
+      chatLogContent: ''
     }
   },
   mounted: function () {
     this.chatSocket = new WebSocket(
       'ws://' + this.hostAdr + '/ws/chat/' + this.roomName + '/')
-
-    this.chatSocket.onmessage = function (e) {
-      var data = JSON.parse(e.data)
-      var message = data['message']
-      document.querySelector('#chat-log').value += (message + '\n')
-      console.log('message:' + message)
-    }
-
-    this.chatSocket.onclose = function (e) {
-      console.error('Chat socket closed unexpectedly')
-      console.log('close:')
-    }
+    this.chatSocket.onmessage = this.onMessage
+    this.chatSocket.onclose = this.onClose
   },
   methods: {
     sendMessage: function () {
@@ -49,6 +40,23 @@ export default {
       }))
       console.log('send: ' + this.inputContent)
       this.inputContent = ''
+    },
+    onMessage: function (e) {
+      var data = JSON.parse(e.data)
+      var message = data['message']
+      this.chatLogContent += ('> ' + message + '\n')
+      console.log('message:' + message)
+    },
+    onClose: function (e) {
+      console.error('Chat socket closed unexpectedly')
+      console.log('close:')
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
     }
   }
 }
@@ -62,7 +70,9 @@ export default {
   height: 600px;
 }
 
-textarea {
+#chatLog {
   width: 150%;
+  overflow-x: hidden;
+  overflow-y: scroll;
 }
 </style>
