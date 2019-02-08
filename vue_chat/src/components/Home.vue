f151df1371026a72be438b08bb6c1a2493afb0a8
zondaTW <u226699@gmail.com>
2019-02-08 11:42:12 +0800

    Add home

---
+++ vue_chat/src/components/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="homeSection" class="Room container">
+    <div class="row h-15">
+      <div class="col" >
+        <h1><span class="badge badge-primary">Join ?</span></h1>
+      </div>
+    </div>
+    <div class="row h-10">
+      <input
+        ref="inputField"
+        type="text"
+        class="w-75"
+        v-focus
+        placeholder="Room name"
+      />
+      <button
+        type="button"
+        class="btn btn-primary w-25"
+      >Join</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  mounted: function () {
+  },
+  methods: {
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#homeSection {
+  min-width: 300px;
+  width: 30%;
+  height: 600px;
+}
+
+</style>
--- vue_chat/src/router/index.js
+++ vue_chat/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Room from '@/components/Room'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
     {
       path: '/room',
       name: 'Room',
